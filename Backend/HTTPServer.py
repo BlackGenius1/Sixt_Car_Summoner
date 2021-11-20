8ab36fef6ad6e3819de4b086e285405732731e87
dominjack <schiwietzdominik0@gmail.com>
2021-11-20 02:11:12 +0100

    server1

--- Backend/HTTPServer.py
+++ Backend/HTTPServer.py
@@ -2,21 +2,30 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 PORT = 8000
 
-tasklist = ["t1", "t2", "t3"]
+tasklist = ['t1', 't2', 't3']
 
 
 class requestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("content", "text/html")
+        self.send_header('content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(self.path[1:].encode())
+        #self.wfile.write(self.path[1:].encode())
 
-        output = ""
+        output = ''
+        output+= '<html><body>'
+        output+= '<h1> Task List</h1>'
+        for task in tasklist:
+            output+= task
+            output += '</br>'
+        output+= '</body></html>'
+        print(output)
+        self.wfile.write(output.encode())
+
 
 
 def main():
-    server = HTTPServer(("", PORT), requestHandler)
+    server = HTTPServer(('', PORT), requestHandler)
     print(f"Server running on port {PORT}")
     server.serve_forever()
 
