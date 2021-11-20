c1d0118bb9b1b82efb885e7272f36db6585d7d35
Dominik Schiwietz <60383740+dominjack@users.noreply.github.com>
2021-11-20 18:11:08 +0100

    Update HTTP_Server.py

--- Backend/HTTP_Server.py
+++ Backend/HTTP_Server.py
@@ -23,7 +23,8 @@ GEOFENCE_STEP = .01
 
 users = [{'uid': '1111111111111111'}]
 google_maps_api_key = 'AIzaSyCYqNsvXY_BsymUGlLK2QFRuZvAKsR2YEg'
-jobs = {}
+potential_jobs = []
+jobs = []
 
 
 def getVehicles():
@@ -49,10 +50,7 @@ def updateBatteryChargeOfVehicle(id,charge):
         res = requests.post(f'https://us-central1-sixt-hackatum-2021.cloudfunctions.net/api/vehicles/{id}/charge', json={"charge": charge},headers = {'Content-type': 'application/json', 'Accept': 'text/plain'})
 
 
-
-
 """Sort the vehicles using the googlemaps api"""
-
 
 def getRouteInfo(start, destination):
     """Get route information from the googlemaps api"""
@@ -62,27 +60,33 @@ def getRouteInfo(start, destination):
     return route
 
 def getRouteLength(start, final_destination):
+    """Return the length of a given route"""
     route = getRouteInfo(start, final_destination)
     return route['rows'][0]['elements'][0]['distance']['value'] /1000
 
 def isInGeofence(destination, vehicle, geofence):
+    """Retrurn true if the vehicle is inside of a geofence"""
     return abs(destination[0] - vehicle['lat']) < geofence and abs(destination[1] - vehicle['lng']) < geofence
 
 def isEnoughCharge(final_destination, destination, vehicle):
+    """Return Trrue if the vehicle has enough Battery left to make the ride"""
     start = (vehicle['lat'], vehicle['lng'])
     distance = getRouteLength(start, destination) + getRouteLength(destination, final_destination)
     needed_charge = distance/KILOMETERS_PER_PERCENT * 1.1
     return vehicle['charge'] >= needed_charge
 
 def filterFREEVehicles(vehicles):
+    """Retrun all vehicles with status FREE"""
     return list(filter(lambda x: x['status'] == 'FREE', vehicles))
 
 def prefilterVehicles(destination, vehicles, geofence):
+    """Return all vehicles wich have status FREE and are in a geofence"""
     vehicles = list(filter(lambda x: x['status'] == 'FREE', vehicles))
     vehicles = list(filter(lambda x: isInGeofence(destination, x, geofence), vehicles))
     return vehicles
 
 def postfilterVehicles(final_destination, destination, vehicles):
+    """Returens all vehicles with enough charge to make the ride"""
     vehicles = list(filter(lambda x: isEnoughCharge(final_destination, destination, x), vehicles))
     return vehicles
 
@@ -120,59 +124,83 @@ def SortVehicles(final_destination, destination, vehicles):
     return vehicles
 
 def getBestVehicle(final_destination, destination, vehicles):
+    """Return most suited vehicle"""
     sorted = SortVehicles(final_destination, destination, vehicles)
     if sorted:
         return sorted[0]
     else:
         return 
 
+def createJob(start, destination, uid, vehicleID):
+    """Return job dictionary"""
+    job = {'lat1': start[0], 'lng1': start[1], 'lat2': destination[0], 'lng2': destination[1], 'uid': uid, 'vehicleID': vehicleID}
+    return job
+
 class requestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        
-        if self.path[:9]=='/confirm':
-            self.send_response(200)
-            self.send_header('content-type', 'text/html')
-            self.end_headers()
-        elif self.path[:7]=='/pickup':
-            self.send_response(200)
-            self.send_header('content-type', 'text/html')
-            self.end_headers()
-        elif self.path[:8]=='/dropoff':
-            self.send_response(200)
-            self.send_header('content-type', 'text/html')
-            self.end_headers()
-        elif self.path[:7]=='/cancel':
-            self.send_response(200)
-            self.send_header('content-type', 'text/html')
-            self.end_headers()
-        else:
-            self.send_error(404,"Error! Invalid URL.")
-            
-    def do_POST(self):
-
-        if self.path[:6]=='/login':
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
-            print(post_data)
+        """Handle GET requests"""
+        if self.path[:6]=='/test':
             self.send_response(200)
             self.send_header('content-type', 'text/html')
             self.end_headers()
             self.wfile.write(json.dumps(filterFREEVehicles(getVehicles())).encode())
 
-        elif self.path[:6]=='/route':
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
-            print(post_data)
+    def do_POST(self):
+        """Handle POST requests"""
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        data = post_data
+
+        if self.path[:6]=='/login':
             self.send_response(200)
             self.send_header('content-type', 'text/html')
             self.end_headers()
-            data = dict(post_data)
-            out = getBestVehicle((data['lat2'], data['lng2']), (data['lat1'], data['lat2'], getVehicles()))
+            self.wfile.write(json.dumps(prefilterVehicles(getVehicles()), .5).encode())
+
+        elif self.path[:6]=='/route':
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+            data = post_data
+            out = getBestVehicle((data['lat2'], data['lng2']), (data['lat1'], data['lat2']), getVehicles())
             if out:
+                potential_jobs.append(createJob((data['lat1'], data['lng1']),(data['lat2'], data['lng2']), data['uid'], data['vehicleID']))
                 self.wfile.write(out.encode())
             else:
                 msg = 'No fitting car found'
                 self.wfile.write(msg.encode())
+        
+        elif self.path[:9]=='/confirm':
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+            
+            #self.wfile.write() 
+            #TODO: confirm to api
+        
+        elif self.path[:7]=='/pickup':
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+            #self.wfile.write()
+            #TODO: confirm pickup
+        
+        elif self.path[:8]=='/dropoff':
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+            #self.wfile.write()
+            #TODO: confirm dropoff/end job
+
+        elif self.path[:7]=='/cancel':
+            self.send_response(200)
+            self.send_header('content-type', 'text/html')
+            self.end_headers()
+            #self.wfile.write()
+            #TODO: cancel job
+
+        else:
+            self.send_error(404,"Error! Invalid URL.")
 
 
 def main():
@@ -181,7 +209,6 @@ def main():
     server.serve_forever()
 
 if __name__ == "__main__":
-    print(len(getVehicles()))
     main()
     #print(dictionaryFromJson(getVehicles()))
     #vehicles = getVehicles()
